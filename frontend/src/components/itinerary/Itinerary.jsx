ac1e6d66319a6f57b407f290ed4e9583cd144284
[mayra <[mayradelvallegonzalez@gmail.com]>
2022-06-23 12:20:47 -0300

    agregandoItinerarios

--- frontend/src/components/itinerary/Itinerary.jsx
+++ frontend/src/components/itinerary/Itinerary.jsx
@@ -3,7 +3,10 @@ import { useSelector , useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { useEffect } from 'react'
 import itinerariesActiones from '../redux/actions/itinerariesActions'
- 
+import { Card } from '@nextui-org/react';
+import { Card, Col, Row, Button, Text } from "@nextui-org/react";
+
+
 function Itinerary(){
     const {id} = useParams()
     const dispatch = useDispatch()
@@ -12,7 +15,85 @@ function Itinerary(){
     },[])
     const itineraries = useSelector(store => store.itinerariesReducer.getItinerariesByCity)
     return (
-
+        <Card css={{ w: "100%", h: "400px" }}>
+        <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+          <Col>
+            <Text size={12} weight="bold" transform="uppercase" color="#9E9E9E">
+              {itineraries.city}
+            </Text>
+            <Text h3 color="white">
+              {itineraries.name}
+              {itineraries.activities}
+              {itineraries.description}
+            </Text>
+          </Col>
+        </Card.Header>
+        <Card.Body css={{ p: 0 }}>
+          <Card.Image
+            src="" //colocar imagen
+            objectFit="cover"
+            width="100%"
+            height="100%"
+            alt="Itinerary Background"
+          />
+        </Card.Body>
+        <Card.Footer
+          isBlurred
+          css={{
+            position: "absolute",
+            bgBlur: "#0f111466",
+            borderTop: "$borderWeights$light solid $gray800",
+            bottom: 0,
+            zIndex: 1,
+          }}
+        >
+          <Row>
+            <Col>
+              <Row>
+                <Col span={3}>
+                  <Card.Image
+                    src= {itineraries.image}
+                    css={{ bg: "black", br: "50%" }}
+                    height={40}
+                    width={40}
+                    alt={itineraries.nameUser}
+                  />
+                </Col>
+                <Col>
+                  <Text color="#d1d1d1" size={12}>
+                    {itineraries.duration}
+                    {itineraries.price}
+                   
+                  </Text>
+                  <Text color="#d1d1d1" size={12}>
+                    {itineraries.hashtags}
+                    {itineraries.likes}
+                  </Text>
+                </Col>
+              </Row>
+            </Col>
+            <Col>
+              <Row justify="flex-end">
+                <Button
+                  flat
+                  auto
+                  rounded
+                  css={{ color: "#94f9f0", bg: "#94f9f026" }}
+                >
+                  <Text
+                    css={{ color: "inherit" }}
+                    size={12}
+                    weight="bold"
+                    transform="uppercase"
+                  >
+                    Get App
+                  </Text>
+                </Button>
+              </Row>
+            </Col>
+          </Row>
+        </Card.Footer>
+      </Card>
     )
 }
 
