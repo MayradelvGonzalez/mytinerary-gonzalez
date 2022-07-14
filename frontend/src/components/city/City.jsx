2f91e08a1a412cc1f6e03e31f7c9e25c2fe8f624
[mayra <[mayradelvallegonzalez@gmail.com]>
2022-07-14 16:15:43 -0300

    comit

--- frontend/src/components/city/City.jsx
+++ frontend/src/components/city/City.jsx
@@ -2,34 +2,36 @@ import React, { useState } from 'react';
 import SearchPage from '../searchPage/SearchPage';
 import NotFound from '../notFound/NotFound';
 import { connect } from 'react-redux'
-import {Animated} from "react-animated-css";
+import { Animated } from "react-animated-css";
 
-function Cities(props){
+function Cities(props) {
 
-    const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
 
-    let filterInput = props.cities?.filter((city) => city.name.toLowerCase().startsWith(inputValue.toLowerCase().trim()));
-     return (
-      <> 
+  let filterInput = props.cities?.filter((city) => city.name.toLowerCase().startsWith(inputValue.toLowerCase().trim()));
+  return (
+    <>    <div className="cajaInput">
       <div className="containerInput">
-       <h3 className="buscadorTitulo">Search your favorite City!</h3>
-       
-      <input onKeyUp={
-            (evento )=>{setInputValue(evento.target.value)}} type="text" placeholder='search city...' >
-         </input>  
-         </div>
-   
-        <div className="cardsBox">
-            {filterInput?.length > 0 ? (<SearchPage cardFilter={filterInput} />) : (<NotFound />) }
-        </div>
-      </>
-       );}
-       
-  const mapStateToProps = (state) => {
-    return {
-        cities:state.citiesReducer.cities,
-        auxiliar:state.citiesReducer.auxiliar
-        }
-      }
-       
-export default connect(mapStateToProps,null)(Cities);
+        <h3 className="buscadorTitulo">Search your favorite City!</h3>
+
+        <input onKeyUp={
+          (evento) => { setInputValue(evento.target.value) }} type="text" placeholder='search city...' >
+        </input>
+      </div>
+    </div>
+
+      <div className="cardsBox">
+        {filterInput?.length > 0 ? (<SearchPage cardFilter={filterInput} />) : (<NotFound />)}
+      </div>
+    </>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    cities: state.citiesReducer.cities,
+    auxiliar: state.citiesReducer.auxiliar
+  }
+}
+
+export default connect(mapStateToProps, null)(Cities);
