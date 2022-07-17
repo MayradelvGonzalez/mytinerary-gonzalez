8dab0e59c27030cb0595688e0443778c90233084
[mayra <[mayradelvallegonzalez@gmail.com]>
2022-07-17 16:17:20 -0300

    comit

--- frontend/src/components/landing/Home.jsx
+++ frontend/src/components/landing/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link as LinkRouter } from 'react-router-dom'
 import Carrousel from '../carrousel/Carrousel'
-import {Animated} from "react-animated-css";
+import { Animated } from "react-animated-css";
 
 function Home() {
 
@@ -10,14 +10,14 @@ function Home() {
       <div className="container-landing">
         <div className="landing">
           <div className="container-titulos">
-          <h1 className='titulo'>Mytinerary</h1>
-          <Animated animationIn="zoomIn" animationOut="fadeOut" isVisible={true}><h2>Find your perfect trip, designed by insiders who know and love their cities!</h2></Animated>
+            <h1 className='titulo'>Mytinerary</h1>
+            <Animated animationIn="zoomIn" animationOut="fadeOut" isVisible={true}><h2>Find your perfect trip, designed by insiders who know and love their cities!</h2></Animated>
           </div>
         </div>
         <div className='container-boton'>
           <div>
             <LinkRouter to='/cities' className="searchpage">
-              <button className="botonCall">Click here!</button>
+              <Animated animationIn="tada" animationOut="fadeOut" isVisible={true}><button className="botonCall">Click here!</button></Animated>
             </LinkRouter>
           </div>
         </div>
